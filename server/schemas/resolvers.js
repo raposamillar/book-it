53c01d38cc2f9ca5e1dcfbe39173dd0a138b2012
raposamillar <lisa.raposamillar@gmail.com>
2022-11-25 22:56:16 -0500

    In resolvers, create nested functions for addUser and login Mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 
 const resolvers = {
@@ -7,7 +8,29 @@ const resolvers = {
         .select('-__v -password')
         .populate('savedBooks');
     },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+
+        return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      return user;
+    }
   }
-}
+};
 
 module.exports = resolvers;
