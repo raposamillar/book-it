ebda85914a325e0f2b97ad973e5583c6f3d5900d
raposamillar <lisa.raposamillar@gmail.com>
2022-11-27 14:32:44 -0500

    in SavedBooks.js, lines 47-76, edit the try/catch block and add the bookToRemove variable on line 49

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -45,6 +45,10 @@ const SavedBooks = () => {
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
+    // use filter to exclude books that have not been removed?
+    const bookToRemove = searchedBooks.filter((book) => book.bookId === bookId);
+
+    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -52,25 +56,29 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      await removeBook({
+        variables: { id: bookToRemove.bookId }
+      });  
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      setRemovedBookIds([...removedBookIds, bookToRemove.bookId]);
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
+      // // upon success, remove book's id from localStorage
+      // removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+  // if (!userDataLength) {
+  //   return <h2>LOADING...</h2>;
+  // }
 
   return (
     <>
