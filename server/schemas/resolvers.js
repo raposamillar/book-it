e0f44212b82ede7bbb6fc1c7303d7760975bc2b3
raposamillar <lisa.raposamillar@gmail.com>
2022-11-29 13:56:10 -0500

    in resolvers.js, line 3, remove Book since it is a subschema of User

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { default: SavedBooks } = require('../../client/src/pages/SavedBooks');
-const { User, Book } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
