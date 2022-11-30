59497339876dc17409fc758705295778e145be5a
raposamillar <lisa.raposamillar@gmail.com>
2022-11-30 12:55:38 -0500

    LoginForm.js, lines 6-7, 15, and 36-48, import LOGIN_USER and run mutation with auth

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,11 +1,19 @@
-// see SignupForm.js for comments
+// see SignupForm.js for additional comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import mutation
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+
+  // create loginUser variable to use the mutation `LOGIN_USER`
+  const [loginUser] = useMutation(LOGIN_USER);
+
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,26 +34,36 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      // const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // pass in data variable from form and run the loginUser mutation
+      const { data } = await loginUser({
+        variables: { ...userFormData }
+      });
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+      Auth.login(data.loginUser.token);
+    } catch (e) {
+      console.error(e);
+    }  
   };
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
+
+    // setUserFormData({
+    //   username: '',
+    //   email: '',
+    //   password: '',
+    // });
+  // };
 
   return (
     <>
